f520e2b01cf17e1fc2a33b187f15703f6c32d5a0
KLee <kevinlee05@gmail.com>
2017-09-16 10:40:35 +0000

    added site feed and sitemap

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,21 @@
 from django.conf.urls import url
 from . import views
+from .feeds import LatestPostsFeed
 
 urlpatterns =[
-    #post views
-    url(r'^list_functional/$', views.post_list_functional, name='post_list_functional'),
-    url(r'^$', views.PostListView.as_view(), name='post_list'),
+    #post views - functional view
+    url(r'^$', views.post_list_functional, name='post_list_functional'),
+    #class based list view
+    url(r'^list_class_based$', views.PostListView.as_view(), name='post_list'),
+    # post list filtered by tag
     url(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list_functional, name='post_list_by_tag'),
+    # individual post view
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'r'(?P<post>[-\w]+)/$',
         views.post_detail,
         name='post_detail'),
+    # share post view
     url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
+    # latest posts feed view
+    url(r'^feed/$', LatestPostsFeed(), name='post_feed'),
 
 ]
