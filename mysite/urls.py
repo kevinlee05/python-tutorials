7228b094160dd0b47dccb0c49c5b2cf236ecbf61
KLee <kevinlee05@gmail.com>
2017-10-31 10:40:23 +0000

    translate urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,30 +1,17 @@
-"""mysite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
 from django.conf.urls import include, url
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
 from django.conf import settings
 from django.conf.urls.static import static
+from django.utils.translation import gettext_lazy as _
 
 sitemaps = {
     'posts': PostSitemap
 }
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^account/', include('account.urls')),
     url(r'^images/', include('images.urls', namespace='images')),
@@ -32,14 +19,14 @@ urlpatterns = [
     url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog') ),
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps },
         name='django.contrib.sitemaps.views.sitemap'),
-    url(r'^cart/', include('cart.urls', namespace='cart')),
-    url(r'^orders/', include('orders.urls', namespace='orders')),
+    url(_(r'^cart/'), include('cart.urls', namespace='cart')),
+    url(_(r'^orders/'), include('orders.urls', namespace='orders')),
+    url(_(r'^payment/'), include('payment.urls', namespace='payment')),
     url(r'^paypal/', include('paypal.standard.ipn.urls')),
-    url(r'^process/', include('payment.urls', namespace='payment')),
-    url(r'^coupons/', include('coupons.urls', namespace='coupons')),
+    url(_(r'^coupons/'), include('coupons.urls', namespace='coupons')),
     url(r'^rosetta/', include('rosetta.urls')),
     url(r'^', include('shop.urls', namespace='shop')),
-]
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
