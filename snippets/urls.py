f75e8364365741ad0a041d421c0efb7f48ce8f11
kevinlee05 <kevinlee05@gmail.com>
2018-02-06 05:04:08 +0800

    pastebin API

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,17 +1,21 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 from rest_framework import renderers
 
 from snippets import views
 from snippets.views import SnippetViewSet, UserViewSet, api_root
+
+schema_view = get_schema_view(title='Pastebin API')
 
 router = DefaultRouter() #automatically creates API root view
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls))
+    url(r'^schema/$', schema_view),
+    url(r'^', include(router.urls)),
 ]
 
 #manual viewset view definitions
